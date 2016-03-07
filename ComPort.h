4cfa2c6225b8cfc81a479caed2414cb81f024166
Chistov Maxim <Chistov.m.a@gmail.com>
2016-03-07 15:59:37 +0300

    заглушка для сборки unix

--- ComPort.h
+++ ComPort.h
@@ -1,10 +1,127 @@
 #pragma once
 
+#ifdef WINDOWS
 #ifndef NOMINMAX
     #define NOMINMAX //иначе API windows определит макросы min и max, конфликтующие с std::max и std::min в vector
 #endif
 
 #include <windows.h>
+#endif
+
+#if defined( DEMO ) && not defined( WINDOWS )
+
+#define CBR_110 110
+#define CBR_300 300
+#define CBR_600 600
+#define CBR_1200 1200
+#define CBR_2400 2400
+#define CBR_4800 4800
+#define CBR_9600 9600
+#define CBR_14400 14400
+#define CBR_19200 19200
+#define CBR_38400 38400
+
+#define CBR_56000 56000
+#define CBR_57600 57600
+#define CBR_115200 115200
+#define CBR_128000 128000
+#define CBR_256000 256000
+
+#define TRUE 1
+#define FALSE 0
+
+#define NOPARITY 0
+#define ODDPARITY 1
+#define EVENPARITY 2
+#define MARKPARITY 3
+#define SPACEPARITY 4
+
+#define ONESTOPBIT 1
+#define ONE5STOPBITS 2
+#define TWOSTOPBITS 3
+
+#define EV_RXCHAR 0 	// Any Character received
+#define EV_RXFLAG 1 	// Received certain character
+#define EV_TXEMPTY 2 	// Transmitt Queue Empty
+#define EV_CTS 3          // CTS changed state
+#define EV_DSR 4          // DSR changed state
+#define EV_RLSD 5        // RLSD changed state
+#define EV_BREAK 6      // BREAK received
+#define EV_ERR 7          // Line status error occurred
+#define EV_RING 8        // Ring signal detected
+#define EV_PERR 9        // Printer error occured
+#define EV_RX80FULL 10// Receive buffer is 80 percent full
+#define EV_EVENT1 11    // Provider specific event 1
+#define EV_EVENT2 12    // Provider specific event 2
+
+#define PURGE_TXABORT 0  // Kill the pending/current writes to the comm port.
+#define PURGE_RXABORT 1  // Kill the pending/current reads to the comm port.
+#define PURGE_TXCLEAR 2  // Kill the transmit queue if there.
+#define PURGE_RXCLEAR 3  // Kill the typeahead buffer if there.
+
+typedef int DWORD;
+typedef unsigned char BYTE;
+
+typedef int HANDLE;
+#define DTR_CONTROL_DISABLE 0
+#define RTS_CONTROL_DISABLE 0
+
+#define GENERIC_READ 0
+#define GENERIC_WRITE 1
+#define OPEN_EXISTING 0
+#define FILE_ATTRIBUTE_NORMAL 0
+
+#define CLRDTR 0
+#define SETDTR 1
+
+
+struct COMMTIMEOUTS
+{
+    int ReadIntervalTimeout;
+    int ReadTotalTimeoutMultiplier;
+    int ReadTotalTimeoutConstant;
+    int WriteTotalTimeoutMultiplier;
+    int WriteTotalTimeoutConstant;
+};
+
+struct DCB
+{
+    int BaudRate;
+    int ByteSize;
+    int Parity;
+    int StopBits;
+    int fAbortOnError;
+    int fDtrControl;
+    int fRtsControl;
+    int fBinary;
+    int fParity;
+    int fInX;
+    int fOutX;
+    BYTE XonChar;
+    BYTE XoffChar;
+    int fErrorChar;
+    int fNull;
+    int fOutxCtsFlow;
+    int fOutxDsrFlow;
+    int XonLim;
+    int XoffLim;
+    int fDsrSensitivity;
+    int DCBlength;
+};
+
+static HANDLE CreateFileA( char const*, int, int, void*, int, int, void* ){ return 1;}
+static void SetCommMask( HANDLE, int ){}
+static void SetupComm( HANDLE, int, int ){}
+static bool SetCommTimeouts( HANDLE, COMMTIMEOUTS* ){ return true; }
+static bool CloseHandle( HANDLE ){ return true; }
+static bool SetCommState( HANDLE, DCB* ) {return true;}
+static bool GetCommState( HANDLE, DCB* ) {return true;}
+static bool EscapeCommFunction( HANDLE, DWORD ) {return true;}
+static bool PurgeComm( HANDLE, DWORD ) {return true;}
+static bool WriteFile( HANDLE, BYTE const*, DWORD, DWORD*, void* ){return true;}
+static bool ReadFile( HANDLE, BYTE*, DWORD, DWORD*, void* ){return true;}
+#endif
+
 #include <string>
 #include <memory>
 using namespace std;
@@ -19,6 +136,7 @@ public:
               unsigned int InputBuffer,
               unsigned int Outputbuffer);
     ~COMPort ();
+
     enum BaudRates	{
         BR_75=75,
         BR_110=CBR_110,
@@ -41,6 +159,7 @@ public:
         BR_128000=CBR_128000,
         BR_256000=CBR_256000
     };
+
     enum Paritys	{
         NOparity=NOPARITY,
         ODDparity=ODDPARITY,
